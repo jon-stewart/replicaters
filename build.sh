8ad2e2432ac984292f7da0dc7bf2ff8fc7024bb4
jon-stewart <jon.linux@ssl-mail.com>
2014-08-17 14:32:03 +0100

    rename of disas to binextract

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ nasm -f elf64 -o /tmp/germ.o -I src/asm/ src/asm/germ.asm
 
 ld -Ttext 0 -o /tmp/germ /tmp/germ.o
 
-python ../disas/disas.py -s /tmp/germ
+python ../binextract/binextract.py -s /tmp/germ
