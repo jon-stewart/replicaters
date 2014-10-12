bb5eefe9fd73310ceba71fe6d30bb12b6c39d50c
jon-stewart <jon.linux@ssl-mail.com>
2014-10-12 16:20:01 +0100

    * flexible enable/disable of debug output from germ asm

--- build.sh
+++ build.sh
@@ -4,7 +4,16 @@ make clean
 
 make
 
-nasm -f elf64 -o /tmp/germ.o -I src/asm/ src/asm/germ.asm
+debug=""
+if [ -n $1 ]
+then
+    if [ "$1" == "debug" ]
+    then
+        debug="-DDEBUG"
+    fi
+fi
+
+nasm -f elf64 -o /tmp/germ.o -I src/asm/ src/asm/germ.asm $debug
 
 ld -Ttext 0 -o /tmp/germ /tmp/germ.o
 
